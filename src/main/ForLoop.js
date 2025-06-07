0b3c6f7ae00f5ca3f175e884df1e9bc7adef50de
Shen Tang <adatang23@yahoo.com>
2025-06-07 07:41:18 +0000

    completed the JavaScript - For Loop Lab

--- src/main/ForLoop.js
+++ src/main/ForLoop.js
@@ -26,6 +26,11 @@ button.onclick = getSum;
  */
 function getSum(){
     let val = parseInt(input.value);
+    let res = 0;
+    for (let i = 0; i < val; i++) {
+        res += i;
+    }
+    result.innerText = res;
 }
 
 
